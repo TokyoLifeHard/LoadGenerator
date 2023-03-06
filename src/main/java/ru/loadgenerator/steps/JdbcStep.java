417ceb23c7ca8faf81a33b41d1f16f58caf7afe6
Kawaiiskii <antonzhadnov@gmail.com>
2023-03-06 20:02:15 +0300

    add JdbcStep

---
+++ src/main/java/ru/loadgenerator/steps/JdbcStep.java
@@ -0,0 +1,47 @@
+package ru.loadgenerator.steps;
+
+import ru.loadgenerator.credentials.DataBaseCredentials;
+
+import java.sql.*;
+
+public class JdbcStep extends Step implements Runnable{
+    DataBaseCredentials dataBaseCredentials;
+
+    public JdbcStep(DataBaseCredentials dataBaseCredentials) {
+        this.dataBaseCredentials = dataBaseCredentials;
+    }
+
+    @Override
+    public void execute() {
+        ResultSet rs = executeQuery();
+        System.out.println(rs.toString());
+        try {
+            rs.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void run() {
+        execute();
+    }
+
+    private Connection createConnecton() throws SQLException {
+        return DriverManager.getConnection(this.dataBaseCredentials.getDataBaseName(),
+                this.dataBaseCredentials.getUser(),
+                this.dataBaseCredentials.getPassword());
+    }
+
+    private ResultSet executeQuery(){
+        ResultSet rs = null;
+
+        try (Connection con = createConnecton()) {
+            PreparedStatement ps = con.prepareStatement(this.dataBaseCredentials.getQuery());
+            rs = ps.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+}
